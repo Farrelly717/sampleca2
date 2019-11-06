7866d325d15bf857adb35cabfe4122d8a7fbf0b9
John Farrelly <S00188044@mail.itsligo.ie>
2019-11-06 20:01:20 +0000

    Add project files.

---
+++ ConsoleApp3/Member.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Member
+    {
+        //properties(take out set to make readonly)
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string MemberID { get; set; }
+        public string Membershipstatus { get; set; }
+        public decimal Price { get; }
+
+        public static int MemberCount { get; set; }
+        //Constructors
+        public Member(string firstname, string lastname, int age, string memberID)
+        {
+            FirstName = firstname;
+        
+            LastName = lastname;
+            Age = age;
+            MemberID = memberID;
+
+            Membershipstatus = "Expired";
+            if (age < 18)
+                Price = 50m;
+            else
+                Price = 150m;
+            MemberCount++;
+        }
+
+        //Methods
+        public override string ToString()
+        {
+            return string.Format("Firstname: {0} Surname: {1} Age: {2} ID: {3} Status: {4} Price: {5}",
+            FirstName, LastName,Age,MemberID,Membershipstatus, Price);
+        }
+        //change status of a member
+        public void ChangeStatus()
+        {
+            //if status is expired change to current
+
+            if (Membershipstatus == "Expired")
+                Membershipstatus = "Current";
+            //if current change to expired
+            else
+                Membershipstatus = "Expired";
+
+           
+
+            //end of method
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Membertest1
+    {
+        static void Main(string[] args)
+        {
+            {
+                Member m1 = new Member("Joe", "Smith", 21, "1234s");
+                Member m2 = new Member("Jane", "Smith", 41, "321A");
+                Member m3 = new Member("Jim", "Smith", 45, "4568A");
+                Member m4 = new Member("Janice", "Smith", 12, "4568C");
+                Member m5 = new Member("John", "Smith", 61, "8952Snr");
+
+                Member[] allMembers = new Member[5];
+                allMembers[0] = m1;
+                allMembers[1] = m2;
+                allMembers[2] = m3;
+                allMembers[3] = m4;
+                allMembers[4] = m5;
+
+                DisplayMembers(allMembers);
+
+                m1.ChangeStatus();
+                m3.ChangeStatus();
+                Console.WriteLine();
+                DisplayMembers(allMembers);
+                Console.WriteLine("The total amount of members are:{0}",Member.MemberCount);
+            }
+            
+        }
+        static void DisplayMembers(Member[] allmembers)
+        {
+            foreach (Member m in allmembers)
+            {
+                Console.WriteLine(m);
+            }
+        }
+    }
+}
